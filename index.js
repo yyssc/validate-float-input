6a924b6cd5f140c5f614efa5b376a85673db59f7
Devin Chen <d3vin.chen@gmail.com>
2017-09-07 13:27:25 +0800

    export ValidateFloatInput component

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 // components
 const HelloWorld = require('./lib/components/HelloWorld').default;
+const ValidateFloatInput = require('./lib/components/ValidateFloatInput').default;
 
 // shapes
 const HelloWorldShape = require('./lib/shapes/HelloWorldShape').default;
 
 module.exports = {
   HelloWorld: HelloWorld,
+  ValidateFloatInput: ValidateFloatInput,
 
   HelloWorldShape: HelloWorldShape,
 };
